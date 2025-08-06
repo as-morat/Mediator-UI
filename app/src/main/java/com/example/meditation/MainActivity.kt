15962f762fdcb08b11940e755195a46f7e745abd
as-morat <morat9511@gmail.com>
2025-08-06 17:05:08 +0600

    Initial Update

---
+++ app/src/main/java/com/example/meditation/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.meditation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.meditation.ui.theme.MeditationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MeditationTheme {
+
+            }
+        }
+    }
+}
